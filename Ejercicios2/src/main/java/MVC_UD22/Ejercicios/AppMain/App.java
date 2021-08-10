8f20a69c20f002db849e4aa2660ab64d6b775969
AlvaroVazRod <alvarovazrod@gmail.com>
2021-08-10 10:11:00 +0200

    Primera version

---
+++ Ejercicios2/src/main/java/MVC_UD22/Ejercicios/AppMain/App.java
@@ -0,0 +1,13 @@
+package MVC_UD22.Ejercicios.AppMain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Ejercicios2/src/main/java/MVC_UD22/Ejercicios/models/Modelo.java
@@ -0,0 +1,5 @@
+package MVC_UD22.Ejercicios.models;
+
+public class Modelo {
+
+}
